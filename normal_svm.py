435393225659978138b691847c596d8335f29755
keithlin724 <keithlin724@gmail.com>
2025-05-14 11:04:21 +0800

    update unit api

--- normal_svm.py
+++ normal_svm.py
@@ -24,28 +24,19 @@ pre_precess_data = build_train_test_dataset(
 
 print(pre_precess_data)
 
-# %%
-import numpy as np
-
-label_to_index = {POSITIVE_CLASS: 1, NEGATIVE_CLASS: -1}
-index_to_label = {1: POSITIVE_CLASS, -1: NEGATIVE_CLASS}
-
-
-label_to_index_np = np.vectorize(label_to_index.get)
-index_to_label_np = np.vectorize(index_to_label.get)
 
 # %% Build DataUnit
 data_unit = DataUnit.build_from_dict(
     data_dict=pre_precess_data,
-    label_to_index=label_to_index_np,
-    index_to_label=index_to_label_np,
+    positive_class=POSITIVE_CLASS,
+    negative_class=NEGATIVE_CLASS,
     label="Label",
 )
 print(data_unit)
 # %% test model
 from utils import test_run, TestResult
 
-output = test_run(
+output: list[TestResult] = test_run(
     data_unit=data_unit,
     kernel_name="rbf",
     kernel_arg={"sigma": [5, 1, 0.5, 0.1, 0.05]},
