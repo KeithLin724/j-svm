941a6c7b65be1f65d4217b1d83d32a045482e9b7
keithlin724 <keithlin724@gmail.com>
2025-05-14 14:26:43 +0800

    jsvm test script

---
+++ normal_svm_jax.py
@@ -0,0 +1,50 @@
+# %% load libraries
+from JSVM import SupportVectorMachine
+from data import IrisDataset, build_train_test_dataset, DataUnit
+
+# %% sample dataset
+dataset = IrisDataset.load_iris_file(with_name=True)
+
+# %% peek dataset
+print(dataset.head())
+
+# %% binary classification
+model = SupportVectorMachine(C=10, kernel_name="rbf", kernel_arg={"sigma": 2})
+
+# %% load dataset
+POSITIVE_CLASS = "Setosa"
+NEGATIVE_CLASS = "Virginica"
+
+pre_precess_data = build_train_test_dataset(
+    df_in=dataset,
+    train_size=IrisDataset.TRAIN_DATA_SIZE,
+    positive_class=POSITIVE_CLASS,
+    negative_class=NEGATIVE_CLASS,
+)
+
+print(pre_precess_data)
+
+
+# %% Build DataUnit
+data_unit = DataUnit.build_from_dict(
+    data_dict=pre_precess_data,
+    positive_class=POSITIVE_CLASS,
+    negative_class=NEGATIVE_CLASS,
+    label="Label",
+)
+print(data_unit)
+# %% test model
+from utils import test_run, TestResult
+
+output: list[TestResult] = test_run(
+    data_unit=data_unit,
+    kernel_name="rbf",
+    kernel_arg={"sigma": [5, 1, 0.5, 0.1, 0.05]},
+    C_list=[10],
+    model_type=SupportVectorMachine,
+)
+# %% show result
+print(output)
+
+# train model
+# model.train(x=data_unit.train_x, y=data_unit.train_y)
