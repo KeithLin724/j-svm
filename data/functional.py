e1c2fa6e34ab0bfc1420a4086304f280dad29665
keithlin724 <keithlin724@gmail.com>
2025-05-14 10:41:07 +0800

    support label dynamic name

--- data/functional.py
+++ data/functional.py
@@ -1,17 +1,20 @@
 import pandas as pd
 from typing import Any
 
+from .DataUnit import DataUnit
+
 
 def build_train_test_dataset(
     df_in: pd.DataFrame,
     train_size: int,
     positive_class: Any,
     negative_class: Any,
+    label: str = "Label",
     for_two_fold: bool = False,
 ):
     # have before and after -> for two-fold
-    positive_data = df_in[df_in["Label"] == positive_class]
-    negative_data = df_in[df_in["Label"] == negative_class]
+    positive_data = df_in[df_in[label] == positive_class]
+    negative_data = df_in[df_in[label] == negative_class]
 
     before = [positive_data[:train_size], negative_data[:train_size]]
 
