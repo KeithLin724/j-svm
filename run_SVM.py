890e4bfb28ae07ce0b1d5aeb1119a107bdfa31c7
keithlin724 <keithlin724@gmail.com>
2025-05-17 23:41:38 +0800

    run approx example code

--- run_SVM.py
+++ run_SVM.py
@@ -1,7 +1,4 @@
 # %%
-import os
-
-os.environ["JAX_PLATFORM_NAME"] = "cpu"
 
 
 from sklearn import datasets
@@ -11,12 +8,32 @@ from JSVM import SupportVectorMachine
 from data import DataUnit, build_train_test_dataset
 from rich import print
 import os
+from collections import Counter
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import time
 
 # %%
 adult = fetch_openml(name="adult", version=2, as_frame=True)
 X_adult, y_adult = adult.data, adult.target
 print(f"Adult 資料集: 样本數 = {X_adult.shape[0]}, 特徵維度 = {X_adult.shape[1]}")
 print(set(y_adult))
+print(Counter(y_adult))
+
+## %%
+# 只取正負類各 1000 筆資料
+N = 20000
+pos_mask = y_adult == ">50K"
+neg_mask = y_adult == "<=50K"
+
+X_pos = X_adult[pos_mask].iloc[:N]
+y_pos = y_adult[pos_mask].iloc[:N]
+
+X_neg = X_adult[neg_mask].iloc[:N]
+y_neg = y_adult[neg_mask].iloc[:N]
+
+X_adult = pd.concat([X_pos, X_neg], axis=0).reset_index(drop=True)
+y_adult = pd.concat([y_pos, y_neg], axis=0).reset_index(drop=True)
 
 # %%
 data = build_train_test_dataset(
@@ -27,6 +44,7 @@ data = build_train_test_dataset(
     # label="income",
     # for_two_fold=True,
     return_data_unit=True,
+    scaler=StandardScaler(),
 )
 # %%
 print(data)
@@ -39,11 +57,41 @@ print(data.test_y.shape)
 # %%
 SupportVectorMachine.warm_up()
 model = SupportVectorMachine(
-    C=10, kernel_name="rbf", kernel_arg={"sigma": 0.5}, use_approx=True
+    C=10, kernel_name="rbf", kernel_arg={"sigma": 0.5}, approx_scale=1000
 )
 
 # %%
+start = time.time()
 model.train(data.train_x, data.train_y)
+end = time.time()
 print(model)
+print(f"Training time: {end - start:.2f} seconds")
+
+# %%
+train_acc, _ = model.acc(data.train_x, data.train_y)
+print("Train accuracy:", train_acc)
+
+test_acc, _ = model.acc(data.test_x, data.test_y)
+print("Test accuracy:", test_acc)
+# %%
+# %%
+# SupportVectorMachine.warm_up()
+# model = SupportVectorMachine(
+#     C=10, kernel_name="rbf", kernel_arg={"sigma": 0.5}, approx_scale=10
+# )
+
+# # %%
+# start = time.time()
+# model.train(data.train_x, data.train_y)
+# end = time.time()
+# print(model)
+# print(f"Training time: {end - start:.2f} seconds")
+
+# # %%
+# train_acc, _ = model.acc(data.train_x, data.train_y)
+# print("Train accuracy:", train_acc)
+
+# test_acc, _ = model.acc(data.test_x, data.test_y)
+# print("Test accuracy:", test_acc)
 
 # %%
